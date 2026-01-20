edde758324cc602dff1c7875f454c2c3e5f098bb
hilderonny <hilderonny2014@gmail.com>
2026-01-20 15:06:17 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# arrange-explorer
+
+## Installation
+
+```sh
+sudo docker run --name EXPLORER -d -v ./html:/app/html -p 3000:3000 hilderonny2024/arrange:0.0.1
+```
---
+++ html/js/explorer.js
@@ -0,0 +1,69 @@
+import * as Arrange from '/arrange/js/arrange.js'
+
+let IS_PUBLIC = true
+let CURRENT_PATH = ''
+
+const LIST_DIV = document.getElementById('list')
+const PREVIEW_IFRAME = document.getElementById('preview')
+const PRIVATE_BUTTON = document.getElementById('privatebutton')
+const PUBLIC_BUTTON = document.getElementById('publicbutton')
+
+async function selectDirectoryInCurrentPath(name) {
+    CURRENT_PATH += '/' + name
+    await showCurrentDir()
+}
+
+async function selectFileInCurrentPath(name) {
+    const userPath = IS_PUBLIC ? 'public' : localStorage.getItem('userid')
+    PREVIEW_IFRAME.src = `/api/files/${userPath}/${CURRENT_PATH}/${name}`
+}
+
+async function showCurrentDir() {
+    LIST_DIV.innerHTML = ''
+    PREVIEW_IFRAME.src = ''
+    const functionToCall = IS_PUBLIC ? Arrange.getPublicFile : Arrange.getPrivateFile
+    if (CURRENT_PATH != '') {
+        const parentDiv = document.createElement('div')
+        parentDiv.innerHTML = '..'
+        parentDiv.classList.add('dir')
+        parentDiv.addEventListener('click', async () => {
+            const pathParts = CURRENT_PATH.split('/')
+            pathParts.pop()
+            CURRENT_PATH = pathParts.join('/')
+            await showCurrentDir()
+        })
+        LIST_DIV.appendChild(parentDiv)
+    }
+    const dirContent = await (await functionToCall(CURRENT_PATH)).json()
+    for (const entry of dirContent.sort((a, b) => a.name.localeCompare(b.name))) {
+        const div = document.createElement('div')
+        div.innerHTML = entry.name
+        div.classList.add(entry.type)
+        div.addEventListener('click', async () => {
+            if (entry.type === 'dir') {
+                await selectDirectoryInCurrentPath(entry.name)
+            } else {
+                await selectFileInCurrentPath(entry.name)
+            }
+        })
+        LIST_DIV.appendChild(div)
+    }
+}
+
+PRIVATE_BUTTON.addEventListener('click', async () => {
+    IS_PUBLIC = false
+    CURRENT_PATH = ''
+    PRIVATE_BUTTON.classList.add('active')
+    PUBLIC_BUTTON.classList.remove('active')
+    await showCurrentDir()
+})
+
+PUBLIC_BUTTON.addEventListener('click', async () => {
+    IS_PUBLIC = true
+    CURRENT_PATH = ''
+    PRIVATE_BUTTON.classList.remove('active')
+    PUBLIC_BUTTON.classList.add('active')
+    await showCurrentDir()
+})
+
+showCurrentDir()
